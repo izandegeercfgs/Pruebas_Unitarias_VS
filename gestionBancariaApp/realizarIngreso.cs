fd09439658f53d0aee4dc782399c651f89031692
Izan de Geer <izandegeer.ua@gmail.com>
2025-05-18 12:08:05 +0200

    added gestionBancariaApp

---
+++ gestionBancariaApp/realizarIngreso.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public void realizarIngreso(double cantidad)
+{
+
+    if (cantidad < 0)
+    {
+        mostrarError(ERR_CANTIDAD_INDICADA_NEGATIVA);
+    }
+    else
+    {
+        if (cantidad > 0)
+            saldo -= cantidad; // Error introducido intencionadamente
+    }
+
+}
---
+++ gestionBancariaApp/realizarReintegro.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+public void realizarReintegro(double cantidad)
+{
+
+    if (cantidad <= 0)
+    {
+        mostrarError(ERR_CANTIDAD_INDICADA_NEGATIVA);
+    }
+    else
+    {
+        if (cantidad > 0 && saldo > cantidad)
+        {
+            saldo -= cantidad;
+
+        }
+        else
+            mostrarError(ERR_SALDO_INSUFICIENTE);
+
+    }
+
+}
+
